fa972d56d45169a2ca98389e76ffaa51e9b8dede
isabella g henriques <isabellagh88@gmail.com>
2020-05-20 20:32:06 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,14 +4,8 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-  BRANDS = []
-
   def initialize(brand)
     @brand = brand
-    if BRANDS.include?(brand)
-    else
-      BRANDS << brand
-    end
   end
 
   def cobble
